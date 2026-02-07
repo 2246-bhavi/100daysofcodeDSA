8fb6f59f2302ac6fbacaac5444c4e509d8e1aeb4
Bhavi Chaturvedi <bhavichaturvedi01@gmail.com>
2026-02-07 13:13:28 +0530

    Add files via upload

---
+++ Day4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100];
+    int n, i, temp;
+    int start, end;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    start = 0;
+    end = n - 1;
+
+    while(start < end)
+    {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("Reversed array:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
