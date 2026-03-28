e685c999dd6cb2096881a628b9a0198a6cf08620
Bhavi Chaturvedi <bhavichaturvedi01@gmail.com>
2026-03-28 19:42:59 +0530

    Add files via upload

---
+++ Day56.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* buildTree(int arr[], int n, int i) {
+    if (i >= n || arr[i] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[i]);
+
+    root->left = buildTree(arr, n, 2*i + 1);
+    root->right = buildTree(arr, n, 2*i + 2);
+
+    return root;
+}
+
+int isMirror(struct Node* t1, struct Node* t2) {
+
+    if (t1 == NULL && t2 == NULL)
+        return 1;
+
+    if (t1 == NULL || t2 == NULL)
+        return 0;
+
+    return (t1->data == t2->data) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n, 0);
+
+    if (isMirror(root, root))
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
