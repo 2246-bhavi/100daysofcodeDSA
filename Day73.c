c61bfaab9408811dcfd01e3fe610a4d1fa6943ce
Bhavi Chaturvedi <bhavichaturvedi01@gmail.com>
2026-04-14 08:12:42 +0530

    Add files via upload

---
+++ Day73.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    int freq[26] = {0};
+
+    scanf("%s", s);
+
+    int len = strlen(s);
+
+    // Count frequency
+    for(int i = 0; i < len; i++) {
+        freq[s[i] - 'a']++;
+    }
+
+    // Find first non-repeating
+    for(int i = 0; i < len; i++) {
+        if(freq[s[i] - 'a'] == 1) {
+            printf("%c", s[i]);
+            return 0;
+        }
+    }
+
+    printf("$");
+
+    return 0;
+}
